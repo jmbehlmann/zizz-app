ba6a0cb2a5c7895ed2a6dc80ec15d734d1cbb200
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-01-07 14:13:10 -0600

    relationships, following, followersmodels and controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,22 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  has_many :posts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "leader_id", dependent: :destroy
+  # can change following to leaders
+  has_many :following, through: :active_relationships, source: :leader
+  has_many :followers, through: :passive_relationships
 
+  def follow(other_user)
+    active_relationships.create(leader_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(leader_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_07_164744) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_07_171856) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,16 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_07_164744) do
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "leader_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id", "leader_id"], name: "index_relationships_on_follower_id_and_leader_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    t.index ["leader_id"], name: "index_relationships_on_leader_id"
   end
 
   create_table "users", force: :cascade do |t|
